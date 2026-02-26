8c17e94e327e1a149af8cc40aa8ec2a3125afb6d
Nime Ullaha <nimeullahacse@gmail.com>
2026-02-26 06:51:00 +0600

    get single user api implement

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -6,16 +6,23 @@ const getAllUsers = async(req,res)=>{
 
     try {
             const users = await User.find();
-    res.status(201).json(users)
+    res.status(200).json(users)
     } catch (error) {
         res.status(500).send(error.message)
     }
 };
 
-const getOneUser = (req,res)=>{
+const getOneUser = async(req,res)=>{
+
+    try {
+            const user = await User.findOne({id:req.params.id})
+
     res.status(200).json({
-        message : "Single user "
+        user
     });
+    } catch (error) {
+       res.status(500).json(error.message) 
+    }
 };
 
 const createUser = async(req,res)=>{
