aff4f16c0072a0dee9980dc5056e4b5e9779087a
Nime Ullaha <nimeullahacse@gmail.com>
2026-03-01 01:10:04 +0600

    update api create

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -13,7 +13,6 @@ const getAllUsers = async(req,res)=>{
 };
 
 const getOneUser = async(req,res)=>{
-
     try {
             const user = await User.findOne({id:req.params.id})
 
@@ -41,11 +40,19 @@ const createUser = async(req,res)=>{
         }
 };
 
-const updateUser = (req,res)=>{
+const updateUser = async(req,res)=>{
 
-    res.status(201).json({
-        message : "Update user successfully"
-    });
+    try {
+    const user = await User.findOne({id:req.params.id});
+    user.name = req.body.name;
+    user.age = Number(req.body.age);
+
+    await user.save();
+    res.status(200).json(user);
+    
+    } catch (error) {
+        res.status(500).send(error.message);    
+    }
 };
 
 const deleteUser = async (req,res)=> {
--- routes/user.route.js
+++ routes/user.route.js
@@ -9,6 +9,7 @@ router.get("/",getAllUsers);
 router.post("/:id",getOneUser);
 router.put("/:id",updateUser);
 router.delete("/:id",deleteUser);
+router.patch("/:id",updateUser);
 
 module.exports = router;
 
